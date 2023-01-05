1c26786a5f8e71913f04288082b0bc9cea1446b1
het-tecblic <het.vaghasia@tecblic.com>
2023-01-05 16:10:52 +0530

    celery config

--- elekgo/celery.py
+++ elekgo/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elekgo.settings')
 
-app = Celery('tasks', backend='rpc://localhost', broker='redis://')
+app = Celery('tasks', backend='redis://localhost:6379/0', broker='amqp://')
 
 #for render
 # app = Celery('tasks', backend='redis://red-cep88mh4reb38608up3g:6379', broker='redis://red-cep88mh4reb38608up3g:6379')
