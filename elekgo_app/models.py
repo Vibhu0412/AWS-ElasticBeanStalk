c0b99921705676fdfcfd47059f0c5b69487d2663
het-tecblic <het.vaghasia@tecblic.com>
2023-01-10 17:16:17 +0530

    account_amount default 0

--- elekgo_app/models.py
+++ elekgo_app/models.py
@@ -209,7 +209,7 @@ class PaymentModel(models.Model):
 
 class UserPaymentAccount(models.Model):
     account_user_id = models.ForeignKey(User,on_delete=models.CASCADE)
-    account_amount = models.DecimalField(max_digits=10,decimal_places=2, null=True, blank=True)
+    account_amount = models.DecimalField(max_digits=10,decimal_places=2, default=0, null=True, blank=True)
 
     def __str__(self):
         return str(self.account_user_id)
