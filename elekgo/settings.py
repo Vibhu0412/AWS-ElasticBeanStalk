8fdb87e3f09f7725c605eb43a08d1ef7397e89fa
het-tecblic <het.vaghasia@tecblic.com>
2023-01-12 18:00:11 +0530

    added react intsance ip to cors origin

--- elekgo/settings.py
+++ elekgo/settings.py
@@ -96,6 +96,7 @@ TEMPLATES = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "http://13.126.147.93",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
@@ -111,6 +112,7 @@ CORS_ALLOWED_ORIGINS = [
 
 
 CSRF_TRUSTED_ORIGINS = [
+    "http://13.126.147.93",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
