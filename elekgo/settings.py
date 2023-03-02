82072d50a915ea2da4df118dd07c9e2976f7ae32
het-tecblic <het.vaghasia@tecblic.com>
2023-03-02 16:45:08 +0530

    added allowed host in settings

--- elekgo/settings.py
+++ elekgo/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY =os.getenv('SECRET_KEY')
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.108.233.88', "127.0.0.1"]
+ALLOWED_HOSTS = ['3.108.233.88', "127.0.0.1", "https://main.do85cbgv6z8ah.amplifyapp.com/login"]
 
 
 # Application definition
